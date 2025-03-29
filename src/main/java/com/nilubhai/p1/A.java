7b15f80aa1f2528b404f0461033f8189db536292
nilu <ndebankura01@gmail.com>
2025-03-29 17:36:39 +0530

    first commit

--- src/main/java/com/nilubhai/p1/A.java
+++ src/main/java/com/nilubhai/p1/A.java
@@ -3,5 +3,6 @@ package com.nilubhai.p1;
 public class A {
     public static void main(String[] args) {
         System.out.println("statement 1");
+        System.out.println("statement 2");
     }
 }
