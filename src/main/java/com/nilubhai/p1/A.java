195e95adc1618b660e6879a683d87741df003a0e
Niladri314 <126091593+Niladri314@users.noreply.github.com>
2025-03-29 17:37:52 +0530

    Update A.java

--- src/main/java/com/nilubhai/p1/A.java
+++ src/main/java/com/nilubhai/p1/A.java
@@ -4,5 +4,6 @@ public class A {
     public static void main(String[] args) {
         System.out.println("statement 1");
         System.out.println("statement 2");
+        System.out.println("statement 3");
     }
 }
