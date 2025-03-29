d23c1584687ff3fdd856f6e5ca7f522366fd609a
Niladri314 <126091593+Niladri314@users.noreply.github.com>
2025-03-29 18:06:32 +0530

    Update A.java
    gjbbbb

--- src/main/java/com/nilubhai/p1/A.java
+++ src/main/java/com/nilubhai/p1/A.java
@@ -3,6 +3,7 @@ package com.nilubhai.p1;
 public class A {
     public static void main(String[] args) {
         System.out.println("statement 1");
-      System.out.println("otp added manu lalal");
+        System.out.println("vicky");
+        System.out.println("vetal");
     }
 }
